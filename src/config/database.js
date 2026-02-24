6edab22d0f8cd74db3adc8d232e0a4930f12a49f
Brijesh Kushwaha <brijeshkushwaha386@gmail.com>
2026-02-24 20:29:36 +0530

    added dotenv file

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb+srv://namstedev:EfiaJAU4PEJgHtPB@namstenodejs.uo03xgr.mongodb.net/",
+     (process.env.DB_CONNECTION_SECRET),
       { dbName: "namstedev" }
     );
     console.log("Database connected successfully");
